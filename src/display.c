8c49f21b19857064712f7b8fc3f5c7a4cb5b7fdd
thomaslienbacher <lienbacher.tom@gmail.com>
2018-10-12 20:56:49 +0200

    Added SDL flag for gl context

--- src/display.c
+++ src/display.c
@@ -38,6 +38,8 @@ display_t *display_new(const char *title, int width, int height, char fullscreen
 #ifdef DEBUG_BUILD
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_RELEASE_BEHAVIOR, 0);
+#elif
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_RELEASE_BEHAVIOR, 1);
 #endif
 
     Uint32 flags = SDL_WINDOW_OPENGL | (fullscreen ? SDL_WINDOW_FULLSCREEN : 0);
